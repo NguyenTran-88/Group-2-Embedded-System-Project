fa7654431cfebe613474f55b54ad2fa82018b6b2
Nguyen Tran <trbnguyen88@gmail.com>
2026-04-18 18:53:00 +0700

    Add STM32 firmware folder

---
+++ firmware/stm32/Core/Inc/app.h
@@ -0,0 +1,14 @@
+/*
+ * app.h
+ *
+ */
+
+#ifndef INC_APP_H_
+#define INC_APP_H_
+
+#include "main.h"
+
+void App_Init(UART_HandleTypeDef *huart);
+void App_Loop(void);
+
+#endif /* INC_APP_H_ */
---
+++ firmware/stm32/Debug/Core/Src/ui_assets.d
@@ -0,0 +1,2 @@
+Core/Src/ui_assets.o: ../Core/Src/ui_assets.c ../Core/Inc/ui_assets.h
+../Core/Inc/ui_assets.h:
